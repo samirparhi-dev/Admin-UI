b7a60b62c94008efd5e93b1a2aae13a1372262ad
unknown <DI352929@L-185002740.wipro.com>
2017-09-07 21:04:55 +0530

    wip for category subcategory

--- src/app/category-subcategory-provisioning/category-subcategory-provisioning.component.ts
+++ src/app/category-subcategory-provisioning/category-subcategory-provisioning.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-category-subcategory-provisioning',
-  templateUrl: './category-subcategory-provisioning.component.html',
-  styleUrls: ['./category-subcategory-provisioning.component.css']
+	selector: 'app-category-subcategory-provisioning',
+	templateUrl: './category-subcategory-provisioning.component.html',
+	styleUrls: ['./category-subcategory-provisioning.component.css']
 })
 export class CategorySubcategoryProvisioningComponent implements OnInit {
 
-  constructor() { }
+	// ngmodels
+	state:any;
+	service:any;
+	sub_service:any;
 
-  ngOnInit() {
-  }
+	api_choice: any;
+
+	// flags
+	Add_Category_Subcategory_flag: boolean;
+	showTable: boolean;
+
+
+	constructor() {
+		this.api_choice = "0";
+		this.Add_Category_Subcategory_flag = true;
+		this.showTable = true;
+	}
+
+	ngOnInit() { }
+
+	hideTable()
+	{
+		this.showTable=false;
+	}
+
+	hideForm()
+	{
+		this.showTable = true;
+	}
+
+	changeRequestObject(flag_value)
+	{
+		if (flag_value === "0")
+		{
+			this.Add_Category_Subcategory_flag = true;
+			this.resetFields();
+
+		}
+		if (flag_value === "1")
+		{
+			this.Add_Category_Subcategory_flag = false;
+			this.resetFields();
+		}
+	}
+
+	resetFields(){
+
+	}
 
 }
+
+
