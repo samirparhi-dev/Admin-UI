340b52c87e214357abd94527070fac4fa18ba568
Gursimran Singh (Digital) <GU353199@wipro.com>
2017-09-14 03:07:16 +0530

    config change

--- src/app/services/config/config.service.ts
+++ src/app/services/config/config.service.ts
@@ -8,11 +8,11 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ConfigService {
-    private _commonBaseURL: String = "http://10.152.3.99:8080/CommonV1/";
-    private _helpline1097BaseURL: String = "http://10.152.3.99:8080/helpline1097API/";
-    private _helpline104BaseURL: String = "http://10.152.3.99:8080/helpline104API/";
-    private adminBaseUrl: String = "http://10.152.3.99:8080/adminAPIV1.0/";
-    private superadminBaseURL: String = "http://10.152.3.99:8080/adminAPIV1.0/";
+    private _commonBaseURL: String = "http://14.142.214.242:8080/CommonV1/";
+    private _helpline1097BaseURL: String = "http://14.142.214.242:8080/helpline1097API/";
+    private _helpline104BaseURL: String = "http://14.142.214.242:8080/helpline104API/";
+    private adminBaseUrl: String = "http://14.142.214.242:8080/adminAPIV1.0/";
+    private superadminBaseURL: String = "http://14.142.214.242:8080/adminAPIV1.0/";
     // private _commonBaseURL: String = "http://10.152.3.99:8080/CommonV1/";
     // private _helpline1097BaseURL: String = "http://10.152.3.99:8080/helpline1097API/";
     // private _helpline104BaseURL: String = "http://10.152.3.99:8080/helpline104API/";
