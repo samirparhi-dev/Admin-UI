c3b4b74931d15254e0e0ea2d043e0feab9b3f11a
unknown <DI352929@L-185002740.wipro.com>
2018-01-22 15:31:41 +0530

    contd. sessions and security integration

--- src/app/services/ProviderAdminServices/state-serviceline-role.service.ts
+++ src/app/services/ProviderAdminServices/state-serviceline-role.service.ts
@@ -4,7 +4,8 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import { InterceptedHttp } from '../../http.interceptor';
-import { ConfigService } from "../config/config.service";
+import { SecurityInterceptedHttp } from '../../http.securityinterceptor';
+import { ConfigService } from '../config/config.service';
 
 
 
@@ -26,49 +27,51 @@ export class ProviderAdminRoleService {
 	delete_Role_Url: any;
 	edit_Role_Url: any;
 
-	getFeaturesUrl:any;
+	getFeaturesUrl: any;
 
-	constructor(private http: Http,public basepaths:ConfigService, private httpIntercept: InterceptedHttp) { 
+	constructor(private http: SecurityInterceptedHttp,
+		public basepaths: ConfigService,
+		private httpIntercept: InterceptedHttp) {
 		this.admin_Base_Url = this.basepaths.getAdminBaseUrl();
 		console.log(this.admin_Base_Url);
-		this.get_State_Url = this.admin_Base_Url + "m/role/state";
-		this.get_Service_Url = this.admin_Base_Url + "m/role/service";
-		this.find_Roles_By_State_Service_Url = this.admin_Base_Url + "m/role/search";
-		this.create_Roles_Url = this.admin_Base_Url + "m/role/addRole";
-		this.delete_Role_Url = this.admin_Base_Url + "m/role/deleteRole";
-		this.edit_Role_Url = this.admin_Base_Url + "m/role/editRole";
-		this.getFeaturesUrl=this.admin_Base_Url+"m/searchFeature";
+		this.get_State_Url = this.admin_Base_Url + 'm/role/state';
+		this.get_Service_Url = this.admin_Base_Url + 'm/role/service';
+		this.find_Roles_By_State_Service_Url = this.admin_Base_Url + 'm/role/search';
+		this.create_Roles_Url = this.admin_Base_Url + 'm/role/addRole';
+		this.delete_Role_Url = this.admin_Base_Url + 'm/role/deleteRole';
+		this.edit_Role_Url = this.admin_Base_Url + 'm/role/editRole';
+		this.getFeaturesUrl = this.admin_Base_Url + 'm/searchFeature';
 	};
 
 	getStates(serviceProviderID) {
-		return this.http.post(this.get_State_Url, { "serviceProviderID": serviceProviderID })
+		return this.http.post(this.get_State_Url, { 'serviceProviderID': serviceProviderID })
 			.map(this.handleState_n_ServiceSuccess)
 			.catch(this.handleError);
 	}
 
-	getServices(serviceProviderID,stateID) {
-		return this.http.post(this.get_Service_Url, { "serviceProviderID": serviceProviderID,
-													  "stateID": stateID
-													}).map(this.handleState_n_ServiceSuccess)
-													.catch(this.handleError);
+	getServices(serviceProviderID, stateID) {
+		return this.http.post(this.get_Service_Url, {
+			'serviceProviderID': serviceProviderID,
+			'stateID': stateID
+		}).map(this.handleState_n_ServiceSuccess)
+			.catch(this.handleError);
 	}
 
-	getFeature(serviceID)
-	{
-		return this.http.post(this.getFeaturesUrl, { "serviceID" :serviceID})
-		.map(this.handleSuccess)
-		.catch(this.handleError);
+	getFeature(serviceID) {
+		return this.http.post(this.getFeaturesUrl, { 'serviceID': serviceID })
+			.map(this.handleSuccess)
+			.catch(this.handleError);
 	}
 
-	getRoles(serviceProviderID, stateID,serviceID) {
+	getRoles(serviceProviderID, stateID, serviceID) {
 		return this.httpIntercept.post(this.find_Roles_By_State_Service_Url,
-		{
-			"serviceProviderID": serviceProviderID,
-			"stateID": stateID,
-			"serviceID": serviceID
-		})
-		.map(this.handleSuccess)
-		.catch(this.handleError);
+			{
+				'serviceProviderID': serviceProviderID,
+				'stateID': stateID,
+				'serviceID': serviceID
+			})
+			.map(this.handleSuccess)
+			.catch(this.handleError);
 	}
 
 	createRoles(roles_array) {
@@ -77,36 +80,36 @@ export class ProviderAdminRoleService {
 			.catch(this.handleError);
 	}
 
-	deleteRole(obj)
-	{
+	deleteRole(obj) {
 		return this.httpIntercept.post(this.delete_Role_Url, obj)
 			.map(this.handleSuccess)
 			.catch(this.handleError);
 	}
 
-	editRole(modified_Role_Object)
-	{
+	editRole(modified_Role_Object) {
 		return this.httpIntercept.post(this.edit_Role_Url, modified_Role_Object)
 			.map(this.handleSuccess)
 			.catch(this.handleError);
 	}
 
-	handleSuccess(response: Response) {
-		// console.log((response.json().data).json(), "in service.ts");
-		// console.log(response,"---1");
-		// console.log(response.json(), "---2");
-		console.log(response.json().data, "role service file success response");
-		return response.json().data;
+	handleSuccess(res: Response) {
+		// console.log((response.json().data).json(), 'in service.ts');
+		// console.log(response,'---1');
+		// console.log(response.json(), '---2');
+		console.log(res.json().data, 'role service file success response');
+		if (res.json().data) {
+			return res.json().data;
+		} else {
+			return Observable.throw(res.json());
+		}
 	}
 
 	handleState_n_ServiceSuccess(response: Response) {
-		
-		console.log(response.json().data, "role service file success response");
-		let result=[];
-		result=response.json().data.filter(function(item)
-		{
-			if(item.statusID!=4)
-			{
+
+		console.log(response.json().data, 'role service file success response');
+		let result = [];
+		result = response.json().data.filter(function (item) {
+			if (item.statusID !== 4) {
 				return item;
 			}
 		});
@@ -114,19 +117,10 @@ export class ProviderAdminRoleService {
 	}
 
 	handleError(error: Response | any) {
-		let errMsg: string;
-		if (error instanceof Response) {
-			const body = error.json() || '';
-			const err = body.error || JSON.stringify(body);
-			errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-		} else {
-			errMsg = error.message ? error.message : error.toString();
-		}
-		console.error(errMsg);
-		return Observable.throw(errMsg);
+		return Observable.throw(error.json());
 	}
 
-	
+
 
 
 };
