c1896b9de7908047e691317740d9adcdc2c37168
psharneja <psharneja@gmail.com>
2018-02-22 11:57:07 +0530

    mappind display

--- src/app/procedure-component-mapping/procedure-component-mapping.component.ts
+++ src/app/procedure-component-mapping/procedure-component-mapping.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
 import { dataService } from '../services/dataService/data.service';
-import { ComponentMasterServiceService } from './../services/ProviderAdminServices/component-master-service.service';
+import { ProcedureComponentMappingServiceService } from './../services/ProviderAdminServices/procedure-component-mapping-service.service';
 import { ProviderAdminRoleService } from '../services/ProviderAdminServices/state-serviceline-role.service';
 import { ConfirmationDialogsService } from '../services/dialog/confirmation.service';
 
@@ -26,12 +25,18 @@ export class ProcedureComponentMappingComponent implements OnInit {
   providerServiceMapID: any;
   unfilled: Boolean = false;
   editProcedure: any;
-  componentForm: FormGroup;
+  selectedProcedure: any;
+  selectedComponent: any;
+  selectedComponentList = [];
+  selectedProcedureDescription: any;
+  selectedComponentDescription: any;
+  
+  procedureList: any;
   componentList: any;
+
   constructor(private commonDataService: dataService,
-    private fb: FormBuilder,
     public providerAdminRoleService: ProviderAdminRoleService,
-    private componentMasterServiceService: ComponentMasterServiceService) {
+    private procedureComponentMappingServiceService: ProcedureComponentMappingServiceService) {
     this.states = [];
     this.services = [];
     }
@@ -40,7 +45,9 @@ export class ProcedureComponentMappingComponent implements OnInit {
 
     this.initiateForm();
   }
-
+  consoleValues(event) {
+console.log(this.selectedProcedure, 'value here')
+  }
   /**
  * Initiate Form
 */
@@ -69,7 +76,38 @@ export class ProcedureComponentMappingComponent implements OnInit {
 
     console.log('psmid', ProviderServiceMapID);
     console.log(this.service);
-   //  this.getAvailableComponent();
+
+    this.getProcedureDropDown();
+    this.getComponentDropDown();
+  }
+updateComponentMapList() {
+  this.selectedComponentList.push(this.selectedComponent);
+
+}
+
+  postMappingData() {
+    
+  }
+
+
+  getProcedureDropDown() {
+    this.procedureComponentMappingServiceService
+    .getProceduresList(this.providerServiceMapID)
+    .subscribe(response => this.procedureList = this.successhandeler(response));
+
+  }
+  getComponentDropDown() {
+    this.procedureComponentMappingServiceService
+      .getComponentsList(this.providerServiceMapID)
+      .subscribe(response => this.componentList = this.successhandeler(response));
+
+  }
+
+  procedureSelected() {
+    this.selectedProcedureDescription = this.selectedProcedure.procedureDesc;
+  }
+  componentSelected() {
+    this.selectedComponentDescription = this.selectedComponent.testComponentDesc;
   }
 
   getServices(stateID) {
@@ -82,6 +120,12 @@ export class ProcedureComponentMappingComponent implements OnInit {
     this.service = '';
     this.services = response;
     this.providerServiceMapID = null;
+    this.selectedComponent = '';
+    this.selectedProcedure = '';
+    this.selectedComponentDescription = '';
+    this.selectedProcedureDescription = '';
+    this.procedureList = [];
+    this.componentList = [];
 
   }
   // For State List
