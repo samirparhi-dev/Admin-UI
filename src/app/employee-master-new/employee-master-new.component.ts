8170b1256aab0c3e12b5344e7236b5ca4c5556ca
Janani Venkataraman (Digital) <JA354063@wipro.com>
2018-05-04 01:54:00 +0530

    checked buffer duplication for aadhar,pan and username in employee master and provider admin list

--- src/app/employee-master-new/employee-master-new.component.ts
+++ src/app/employee-master-new/employee-master-new.component.ts
@@ -331,12 +331,13 @@ export class EmployeeMasterNewComponent implements OnInit {
     */
   checkAadhar() {
     console.log('aadharNumber', this.aadharNumber);
-
-    if (this.aadharNumber.length == 12) {
-      this.employeeMasterNewService.validateAadhar(this.aadharNumber).subscribe(
-        (response: any) => {
-          this.checkAadharSuccessHandler(response);
-        }, (err) => this.dialogService.alert(err, 'error'));
+    if (this.aadharNumber != undefined && this.aadharNumber != null) {
+      if (this.aadharNumber.length == 12) {
+        this.employeeMasterNewService.validateAadhar(this.aadharNumber).subscribe(
+          (response: any) => {
+            this.checkAadharSuccessHandler(response);
+          }, (err) => this.dialogService.alert(err, 'error'));
+      }
     }
   }
   checkAadharSuccessHandler(response) {
@@ -352,12 +353,14 @@ export class EmployeeMasterNewComponent implements OnInit {
     * Check Uniqueness in Pan
     */
   checkPan() {
-    if (this.panNumber.length == 10) {
-      this.employeeMasterNewService.validatePan(this.panNumber).subscribe(
-        response => {
-          console.log("pan response", response);
-          this.checkPanSuccessHandler(response);
-        }, (err) => this.dialogService.alert(err, 'error'));
+    if (this.panNumber != undefined && this.panNumber != null) {
+      if (this.panNumber.length == 10) {
+        this.employeeMasterNewService.validatePan(this.panNumber).subscribe(
+          response => {
+            console.log("pan response", response);
+            this.checkPanSuccessHandler(response);
+          }, (err) => this.dialogService.alert(err, 'error'));
+      }
     }
   }
   checkPanSuccessHandler(response) {
@@ -503,6 +506,7 @@ export class EmployeeMasterNewComponent implements OnInit {
     this.checkDuplicatesInBuffer(tempObj);
     this.resetAllForms();
 
+
   }
 
   checkDuplicatesInBuffer(tempObj) {
@@ -528,31 +532,32 @@ export class EmployeeMasterNewComponent implements OnInit {
           duplicateName = duplicateName + 1;
           console.log("this.duplicateName", duplicateName);
         }
-        if (duplicateAadhar === 0 && duplicatePan === 0 && duplicateName === 0) {
-          this.objs.push(tempObj);
-        }
-        if (duplicateAadhar > 0 && duplicatePan > 0 && duplicateName > 0) {
-          this.dialogService.alert("Aadhar, Pan number and Username already exist");
-        }
-        else if (duplicateAadhar > 0 && duplicatePan > 0) {
-          this.dialogService.alert("Aadhar and Pan number already exist");
-        }
-        else if (duplicateAadhar > 0 && duplicateName > 0) {
-          this.dialogService.alert("Aadhar number and Username already exist");
-        }
-        else if (duplicatePan > 0 && duplicateName > 0) {
-          this.dialogService.alert("Pan number and Username already exist");
-        }
-        else if (duplicateAadhar > 0) {
-          this.dialogService.alert("Aadhar number already exist");
-        }
-        else if (duplicatePan > 0) {
-          this.dialogService.alert("Pan number already exist");
-        }
-        else if (duplicateName > 0) {
-          this.dialogService.alert("Already exist");
-        }
       }
+      if (duplicateAadhar === 0 && duplicatePan === 0 && duplicateName === 0) {
+        this.objs.push(tempObj);
+      }
+      else if (duplicateAadhar > 0 && duplicatePan > 0 && duplicateName > 0) {
+        this.dialogService.alert("Aadhar, Pan number and Username already exist");
+      }
+      else if (duplicateAadhar > 0 && duplicatePan > 0) {
+        this.dialogService.alert("Aadhar and Pan number already exist");
+      }
+      else if (duplicateAadhar > 0 && duplicateName > 0) {
+        this.dialogService.alert("Aadhar number and Username already exist");
+      }
+      else if (duplicatePan > 0 && duplicateName > 0) {
+        this.dialogService.alert("Pan number and Username already exist");
+      }
+      else if (duplicateAadhar > 0) {
+        this.dialogService.alert("Aadhar number already exist");
+      }
+      else if (duplicatePan > 0) {
+        this.dialogService.alert("Pan number already exist");
+      }
+      else {
+        this.dialogService.alert("Already exist");
+      }
+
     }
   }
 
@@ -735,7 +740,7 @@ export class EmployeeMasterNewComponent implements OnInit {
   limitDateInEdit(dateOfBirth) {
     console.log("Limit dateOfBirth", dateOfBirth);
     debugger;
-    
+
     this.maxdate = new Date();
     this.maxdate.setFullYear(this.today.getFullYear() - 20);
     this.mindate = new Date();
@@ -790,7 +795,7 @@ export class EmployeeMasterNewComponent implements OnInit {
 
 
   update(userCreationFormValue, demographicsValue, communicationFormValue) {
-   
+
     let update_tempObj = {
       'titleID': userCreationFormValue.title_Id,
       'firstName': userCreationFormValue.user_firstname,
