69a4be9f79839dd31a7397624c2991f5987abcdb
Janani Venkataraman (Digital) <JA354063@wipro.com>
2018-05-31 12:06:25 +0530

    service line filteration added in agent list creation

--- src/app/agent-list-creation/agent-list-creation.component.ts
+++ src/app/agent-list-creation/agent-list-creation.component.ts
@@ -47,6 +47,8 @@ export class AgentListCreationComponent implements OnInit {
   ngOnInit() {
     this.radio_option = '1';
     this.userID = this.commonDataService.uid;
+    console.log("userID", this.
+    userID);
     this.getServices(this.userID);
   }
 
@@ -178,7 +180,7 @@ export class AgentListCreationComponent implements OnInit {
         if (this.resultArray[z].agentID.toString()[0] === "2" ||
           this.resultArray[z].agentID.toString()[0] === "3" ||
           this.resultArray[z].agentID.toString()[0] === "4") {
-          console.log(this.resultArray[z].agentID.toString()[0]);
+          console.log("Validate One",this.resultArray[z].agentID.toString()[0]);
           tick = tick + 1;
         }
       }
