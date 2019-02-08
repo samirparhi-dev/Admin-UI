ae5b9a6a05d7f183ec4c311abbf98156fb194bed
Janani Venkataraman (Digital) <JA354063@wipro.com>
2019-02-08 18:07:16 +0530

    user-mapping for swymed

--- src/app/services/ProviderAdminServices/swymed-user-service.ts
+++ src/app/services/ProviderAdminServices/swymed-user-service.ts
@@ -13,20 +13,68 @@ export class SwymedUserConfigurationService {
 
     providerAdmin_base_url: any;
     getMappedUserDetails: any;
+    getAllDesignationsUrl: any;
+    getUserNameUrl: any;
+    getSwymedDomainUrl: any;
+    saveSwymedUserDetailsUrl: any;
+    updateUserDetailsUrl: any;
+    mappingActivationDeactivationUrl: any;
 
     constructor(private http: InterceptedHttp,
         public basePaths: ConfigService,
         private httpSecurity: SecurityInterceptedHttp) {
 
         this.providerAdmin_base_url = this.basePaths.getAdminBaseUrl();
-        this.getMappedUserDetails = this.providerAdmin_base_url + '/swymed/getmappedUsers/';
+        this.getMappedUserDetails = this.providerAdmin_base_url + 'swymed/getmappedUsers/';
+        this.getAllDesignationsUrl = this.providerAdmin_base_url + 'm/getDesignation';
+        this.getUserNameUrl = this.providerAdmin_base_url + '/swymed/getunmappedUser/';
+        this.getSwymedDomainUrl = this.providerAdmin_base_url + 'swymed/getdomain/';
+        this.saveSwymedUserDetailsUrl = this.providerAdmin_base_url + '/swymed/createUser';
+        this.updateUserDetailsUrl = this.providerAdmin_base_url + '/swymed/editUser';
+        this.mappingActivationDeactivationUrl = this.providerAdmin_base_url + '/swymed/deleteUser/';
     }
 
     getSwymedUserDetails(serviceProviderID) {
         return this.httpSecurity
-            .post(this.getMappedUserDetails, { 'serviceProviderID': serviceProviderID })
+            .post(this.getMappedUserDetails + serviceProviderID, {})
             .map(this.handleSuccess)
             .catch(this.handleError)
+    }
+    getAllDesignations() {
+        return this.httpSecurity
+            .post(this.getAllDesignationsUrl, {})
+            .map(this.handleSuccess_designation)
+            .catch(this.handleError)
+    }
+    getUserName(designationID, serviceProviderID) {
+        return this.httpSecurity
+            .get(this.getUserNameUrl + serviceProviderID + '/' + designationID)
+            .map(this.handleSuccess)
+            .catch(this.handleError)
+    }
+    getSwymedDomain(serviceProviderID) {
+        return this.httpSecurity
+            .post(this.getSwymedDomainUrl + serviceProviderID , { })
+            .map(this.handleSuccess)
+            .catch(this.handleError)
+    }
+    saveSwymedUserDetails(reqObj) {
+        return this.httpSecurity
+            .post(this.saveSwymedUserDetailsUrl, reqObj)
+            .map(this.handleSuccess)
+            .catch(this.handleError)
+    }
+    updateUserDetails(updateObj) {
+        return this.httpSecurity
+        .post(this.updateUserDetailsUrl, updateObj)
+        .map(this.handleSuccess)
+        .catch(this.handleError)
+    }
+    mappingActivationDeactivation(userSwymedMapID, flag, modifiedBy) {
+        return this.httpSecurity
+        .get(this.mappingActivationDeactivationUrl + userSwymedMapID + '/' + flag + '/' + modifiedBy, {})
+        .map(this.handleSuccess)
+        .catch(this.handleError)
     }
 
     handleSuccess(res: Response) {
@@ -36,6 +84,16 @@ export class SwymedUserConfigurationService {
         } else {
             return Observable.throw(res.json());
         }
+    }
+    handleSuccess_designation(res: Response) {
+        console.log(res.json().data, '--- in swymed user config service ');
+        let result = [];
+        result = res.json().data.filter(function (item) {
+            if (item.designationID == 19 || item.designationID == 25 || item.designationID == 26) {
+                return item;
+            }
+        });
+        return result;
     }
 
     handleError(error: Response | any) {
--- src/app/swymed-user-mapping/swymed-user-mapping.component.ts
+++ src/app/swymed-user-mapping/swymed-user-mapping.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { dataService } from '../services/dataService/data.service';
 import { ConfirmationDialogsService } from '../services/dialog/confirmation.service';
 import { SwymedUserConfigurationService } from '../services/ProviderAdminServices/swymed-user-service';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-swymed-user-mapping',
   templateUrl: './swymed-user-mapping.component.html',
@@ -11,33 +12,240 @@ export class SwymedUserMappingComponent implements OnInit {
 
   createdBy: any;
   serviceProviderID: any;
+  designation: any;
+  emailID: any;
+  password: any;
+  username: any;
+  domain: any;
+  dataToBeEdit: any;
+  status: any;
 
   swymedUserDetails: any = [];
+  designations: any = [];
+  userNamesList: any = [];
+  domainList: any = [];
+  filteredswymedUserDetails: any = [];
 
   showTable: Boolean = true;
+  editMode: Boolean = false;
+  disableSelection: Boolean = false;
+
+  dynamictype: any = 'password';
+
+  emailPattern = /^[0-9a-zA-Z_.]+@[a-zA-Z_]+?\.\b(org|com|COM|IN|in|co.in)\b$/;
+  passwordPattern = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,12}$/;
+
+  @ViewChild('mappingForm') mappingForm: NgForm;
 
   constructor(
     public swymedUserConfigService: SwymedUserConfigurationService,
     public dataServiceValue: dataService,
-    public dialogService: ConfirmationDialogsService,
-  ) { }
+    public dialogService: ConfirmationDialogsService) { }
 
   ngOnInit() {
     this.createdBy = this.dataServiceValue.uname;
     this.serviceProviderID = this.dataServiceValue.service_providerID;
     this.getAllSwymedUserDetails();
   }
+  /*
+  * To fetch mapped user details
+  */
   getAllSwymedUserDetails() {
     this.swymedUserConfigService.getSwymedUserDetails(this.serviceProviderID).subscribe(userResponse => {
-      if (userResponse.statusCode == 200) {
+      if (userResponse != undefined) {
         this.swymedUserDetails = userResponse;
+        this.filteredswymedUserDetails = userResponse;
       } else {
-        console.log("error", userResponse.errorMessage);
+        console.log("error");
       }
     })
   }
   showForm() {
     this.showTable = false;
+    this.disableSelection = false;
+    this.editMode = false;
+    this.dataToBeEdit = undefined;
+    this.getAllDesignations();
+    this.getSwymedDomain();
   }
 
+  /*
+  * To fetch Designation
+  */
+  getAllDesignations() {
+    this.swymedUserConfigService.getAllDesignations().subscribe((desigRes) => {
+      this.designations = desigRes;
+      if (this.dataToBeEdit != undefined) {
+        let editDesig = this.designations.filter((editDesigValue) => {
+          if (this.dataToBeEdit.designationName != undefined && this.dataToBeEdit.designationName == editDesigValue.designationName) {
+            return editDesigValue;
+          }
+        })[0]
+        if (editDesig) {
+          this.designation = editDesig;
+        }
+      }
+    })
+  }
+
+  /*
+  * To fetch user list
+  */
+  getUserNameBasedOnDesig(designationID) {
+    this.swymedUserConfigService.getUserName(designationID, this.serviceProviderID)
+      .subscribe(response => {
+        this.userNamesList = response;
+        if (this.dataToBeEdit != undefined) {
+          let editUser = this.userNamesList.filter((editUserValue) => {
+            if (this.dataToBeEdit.userName != undefined && this.dataToBeEdit.userName == editUserValue.userName) {
+              return editUserValue;
+            }
+          })[0]
+          if (editUser) {
+            this.username = editUser;
+          }
+        }
+      })
+  }
+
+  showPWD() {
+    this.dynamictype = 'text';
+  }
+
+  hidePWD() {
+    this.dynamictype = 'password';
+  }
+
+  /*
+  * To fetch swymed domain
+  */
+  getSwymedDomain() {
+    this.swymedUserConfigService.getSwymedDomain(this.serviceProviderID).subscribe((domainResponse) => {
+      this.domainList = domainResponse;
+      if (this.dataToBeEdit != undefined) {
+        let editDomain = this.domainList.filter((editDomainValue) => {
+          if (this.dataToBeEdit.swymedDomain != undefined && this.dataToBeEdit.swymedDomain == editDomainValue.swymedDoamin) {
+            return editDomainValue;
+          }
+        })[0]
+        if (editDomain) {
+          this.domain = editDomain;
+        }
+      }
+    })
+  }
+
+  /*
+  * Save API Call
+  */
+
+  saveUserMapping(formValue) {
+    let saveReqObj = {
+      "userID": formValue.username.userID,
+      "swymedEmailID": formValue.emailID,
+      "swymedPassword": formValue.password,
+      "swymedDomain": formValue.domain.swymedDoamin,
+      "createdBy": this.createdBy,
+
+    }
+    this.swymedUserConfigService.saveSwymedUserDetails(saveReqObj).subscribe((saveResponse) => {
+      this.dialogService.alert("Saved successfully", "success");
+      this.getAllSwymedUserDetails();
+      this.resetForm();
+    })
+  }
+
+  back() {
+    this.dialogService.confirm('Confirm', "Do you really want to cancel? Any unsaved data would be lost").subscribe(res => {
+      if (res) {
+        this.resetForm();
+      }
+    })
+  }
+
+  resetForm() {
+    this.showTable = true;
+    this.editMode = false;
+    this.mappingForm.reset();
+
+  }
+
+  editUserDetails(editValue) {
+    console.log("editValue", editValue);
+    this.dataToBeEdit = editValue;
+    this.disableSelection = true;
+    this.editMode = true;
+    this.showTable = false;
+    this.getAllDesignations();
+    this.getUserNameBasedOnDesig(editValue.designationID);
+    this.getSwymedDomain();
+    this.emailID = editValue.swymedEmailID;
+    this.password = editValue.swymedPassword;
+    
+  }
+  /*
+  * Update API Call
+  */
+
+  updateUsermapping(formValue) {
+    let updateObj = {
+      "swymedEmailID": formValue.emailID,
+      "swymedPassword": formValue.swymedPassword,
+      "swymedDomain": this.dataToBeEdit.swymedDoamin,
+      "userID": this.dataToBeEdit.userID,
+      "userSwymedMapID": this.dataToBeEdit.userSwymedMapID,
+      "modifiedBy": this.createdBy
+    }
+    this.swymedUserConfigService.updateUserDetails(updateObj).subscribe((updateResponse) => {
+      this.dialogService.alert("Updated successfully", "success");
+      this.dataToBeEdit = undefined;
+      this.resetForm();
+    })
+  }
+  /*
+  * Activate or deactivate user mapping 
+  */
+  activateDeactivateMapping(item, userDeleted) {
+    if (userDeleted) {
+      this.dialogService.alert("User is inactive");
+    } else {
+      let flag = !item.Deleted
+      if (flag) {
+        this.status = 'Deactivate';
+      } else {
+        this.status = 'Activate';
+      }
+      this.dialogService.confirm('Confirm', "Are you sure you want to " + this.status + "?").subscribe((res) => {
+        if (res) {
+          this.swymedUserConfigService.mappingActivationDeactivation(item.userSwymedMapID, flag, item.modifiedBy)
+            .subscribe((res) => {
+              console.log('Activation or deactivation response', res);
+              this.dialogService.alert(this.status + "d successfully", 'success');
+              this.getAllSwymedUserDetails();
+            }, (err) => console.log('error', err))
+        }
+      },
+        (err) => {
+          console.log(err);
+        })
+    }
+  }
+  filterComponentList(searchTerm?: string) {
+    if (!searchTerm) {
+      this.filteredswymedUserDetails = this.swymedUserDetails;
+    } else {
+      this.filteredswymedUserDetails = [];
+      this.swymedUserDetails.forEach((item) => {
+        for (let key in item) {
+          if (key == 'userName' || key == 'swymedDomain' || key == 'designationName') {
+            let value: string = '' + item[key];
+            if (value.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0) {
+              this.filteredswymedUserDetails.push(item); break;
+            }
+          }
+        }
+      });
+    }
+
+  }
 }
